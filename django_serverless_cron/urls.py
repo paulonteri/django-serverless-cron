8718414020fe7d79f1ebf26889cef17a7051eec8
Paul Onteri <onteripaul@gmail.com>
2022-01-27 14:35:25 +0300

    add run views

--- django_serverless_cron/urls.py
+++ django_serverless_cron/urls.py
@@ -1,35 +1,14 @@
 # -*- coding: utf-8 -*-
+
+
 from django.conf.urls import url
-from django.views.generic import TemplateView
-
-from . import views
-
+from .views import MyView
 
 app_name = 'django_serverless_cron'
 urlpatterns = [
     url(
-        regex="^CronJobRun/~create/$",
-        view=views.CronJobRunCreateView.as_view(),
-        name='CronJobRun_create',
+        regex="^run/$",
+        view=MyView.as_view(),
+        name='run-serverless-cron-jobs'
     ),
-    url(
-        regex="^CronJobRun/(?P<pk>\d+)/~delete/$",
-        view=views.CronJobRunDeleteView.as_view(),
-        name='CronJobRun_delete',
-    ),
-    url(
-        regex="^CronJobRun/(?P<pk>\d+)/$",
-        view=views.CronJobRunDetailView.as_view(),
-        name='CronJobRun_detail',
-    ),
-    url(
-        regex="^CronJobRun/(?P<pk>\d+)/~update/$",
-        view=views.CronJobRunUpdateView.as_view(),
-        name='CronJobRun_update',
-    ),
-    url(
-        regex="^CronJobRun/$",
-        view=views.CronJobRunListView.as_view(),
-        name='CronJobRun_list',
-    ),
-	]
+]
--- django_serverless_cron/views.py
+++ django_serverless_cron/views.py
@@ -1,38 +1,13 @@
 # -*- coding: utf-8 -*-
-from django.views.generic import (
-    CreateView,
-    DeleteView,
-    DetailView,
-    UpdateView,
-    ListView
-)
 
-from .models import (
-	CronJobRun,
-)
+from django.http import HttpResponse
+from django.views import View
+
+from .services import run_all_jobs
 
 
-class CronJobRunCreateView(CreateView):
+class MyView(View):
 
-    model = CronJobRun
-
-
-class CronJobRunDeleteView(DeleteView):
-
-    model = CronJobRun
-
-
-class CronJobRunDetailView(DetailView):
-
-    model = CronJobRun
-
-
-class CronJobRunUpdateView(UpdateView):
-
-    model = CronJobRun
-
-
-class CronJobRunListView(ListView):
-
-    model = CronJobRun
-
+    def get(self, request, *args, **kwargs):
+        run_all_jobs()
+        return HttpResponse('Done')
