85ea5eb2643f81a66f5db20ece13d872b7ebb347
Paul Onteri <onteripaul@gmail.com>
2022-01-29 09:04:17 +0300

    fix lint issues

--- django_serverless_cron/test_utils/test_app/admin.py
+++ django_serverless_cron/test_utils/test_app/admin.py
@@ -1,3 +1,3 @@
-from django.contrib import admin
+# from django.contrib import admin
 
 # Put your models admin models here
--- tests/example_jobs.py
+++ tests/example_jobs.py
@@ -1,4 +1,8 @@
 
 
-def example_job_1(): pass
-def example_job_2(): pass
+def example_job_1():
+    pass
+
+
+def example_job_2():
+    pass
