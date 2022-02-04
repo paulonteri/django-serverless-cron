76c8240755c95eba70bd1dde2726e4fa7b405a69
WilliamOtieno <jimmywilliamotieno@gmail.com>
2022-02-04 10:44:56 +0300

    Request View / URL
    - Executes deletion through HTTP

--- django_serverless_cron/urls.py
+++ django_serverless_cron/urls.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 
 
-from django.urls import re_path
-from .views import RunJobsView
+from django.urls import re_path, path
+from .views import RunJobsView, purge_jobs_view
 
 app_name = 'django_serverless_cron'
 urlpatterns = [
-    re_path(
-        "^run/$",
-        RunJobsView.as_view(),
-        name='django-serverless-cron-run-jobs'
-    ),
+    re_path("^run/$", RunJobsView.as_view(), name='django-serverless-cron-run-jobs'),
+    path("purge/<num>/", purge_jobs_view, name='django-serverless-cron-purge-jobs'),
 ]
--- django_serverless_cron/views.py
+++ django_serverless_cron/views.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
+import logging
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseServerError, HttpResponseBadRequest
 from django.views import View
 
-from .services import RunJobs
+from .services import RunJobs, purge_jobs
+
+logger = logging.getLogger(__name__)
 
 
 class RunJobsView(View):
@@ -11,3 +14,16 @@ class RunJobsView(View):
     def get(self, request, *args, **kwargs):
         RunJobs.run_all_jobs()
         return HttpResponse('Done')
+
+
+def purge_jobs_view(request, num):
+    n = request.GET.get(num)
+    if n is None:
+        return HttpResponseBadRequest('You must supply the number of jobs to be purged.')
+    try:
+        purge_jobs(n)
+        return HttpResponse(f'Last {n} jobs Purged Successfully.')
+    except Exception as e:
+        logger.exception(e, exc_info=True)
+        logger.error(e)
+        HttpResponseServerError('An error occurred. Working to resolve the issue.')
