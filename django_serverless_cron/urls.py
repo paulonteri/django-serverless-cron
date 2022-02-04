379b36e73b8c3ed0483a854072f9cf9faee4bb35
WilliamOtieno <jimmywilliamotieno@gmail.com>
2022-02-04 11:59:19 +0300

    Purge URL
    - URL for the view

--- django_serverless_cron/urls.py
+++ django_serverless_cron/urls.py
@@ -7,5 +7,5 @@ from .views import RunJobsView, purge_jobs_view
 app_name = 'django_serverless_cron'
 urlpatterns = [
     re_path("^run/$", RunJobsView.as_view(), name='django-serverless-cron-run-jobs'),
-    path("purge/<num>/", purge_jobs_view, name='django-serverless-cron-purge-jobs'),
+    path("purge/<int:n>/", purge_jobs_view, name='django-serverless-cron-purge-jobs'),
 ]
