8fad9b8968a5522d4c5cfcb0dc88f9a2d9509a41
WilliamOtieno <jimmywilliamotieno@gmail.com>
2022-02-04 13:39:28 +0300

    URL
    - CBV url that maps to triggering view

--- django_serverless_cron/urls.py
+++ django_serverless_cron/urls.py
@@ -2,10 +2,10 @@
 
 
 from django.urls import re_path, path
-from .views import RunJobsView, purge_jobs_view
+from .views import RunJobsView, PurgeJobRunsView
 
 app_name = 'django_serverless_cron'
 urlpatterns = [
     re_path("^run/$", RunJobsView.as_view(), name='django-serverless-cron-run-jobs'),
-    path("purge/<int:n>/", purge_jobs_view, name='django-serverless-cron-purge-jobs'),
+    re_path(r'^purge-runs/(?P<n>\w+)/$', PurgeJobRunsView.as_view(), name='django-serverless-cron-purge-jobs'),
 ]
